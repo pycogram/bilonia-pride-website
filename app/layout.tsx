fa585d5bb892ca81eb7761a3ea3ff9fa3a9d85f2
daniel <kaluifesinachi@yahoo.com>
2026-01-28 07:57:16 +0100

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const baseURL = "https://bilonia-pride-v1.vercel.app"; 
+const baseURL = "https://bilonia-pride-v2.vercel.app"; 
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseURL), 
